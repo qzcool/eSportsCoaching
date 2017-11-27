276a956d4925e8a165f37a3c3cffa106f15a1f4d
Qu Xiao Ya <33386378+amelia278@users.noreply.github.com>
2017-11-27 21:28:35 +0800

    update
    update

---
+++ Frontend/gameTutor_Qu/js/calender.js
@@ -0,0 +1,22 @@
+$(function () {
+    $("#datetimepicker1").datetimepicker({
+        format: "yyyy-mm-dd",
+        autoclose: true,
+        todayBtn: true,
+        todayHighlight: true,
+        showMeridian: true,
+        pickerPosition: "bottom-left",
+        language: 'zh-CN',
+        startView: 2,
+        minView: 2
+    });
+    var today = new Date();
+    var nowdate = (today.getFullYear()) + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+    var date = new Date(nowdate);
+    var mon = date.getMonth() + 1;
+    var day = date.getDate();
+    var mydate = date.getFullYear() + "-" + (mon < 10 ? "0" + mon : mon) + "-" + (day < 10 ? "0" + day : day);
+    // document.getElementById("nowdate").value = mydate;
+
+    $(".dropdown-toggle").dropdown('toggle');
+});
--- Frontend/gameTutor_Qu/js/game.js
+++ Frontend/gameTutor_Qu/js/game.js
@@ -131,11 +131,8 @@ function addOptionsForSelect(){
         var img = document.createElement("img");   
         img.src = "images/game" + i + ".png";
         img.classList.add("img-responsive");
-        gamesDiv.appendChild(img);
-      
-    }
-    
-
+        gamesDiv.appendChild(img);    
+    } 
 }
 addOptionsForSelect();
 
@@ -187,24 +184,4 @@ $(function(){
   $("#searchSenseiIndex").click(function () {
       window.location.href = "gamer/searchSensei.html";
   });
-  $("#datetimepicker1").datetimepicker({
-      format: "yyyy-mm-dd",
-      autoclose: true,
-      todayBtn: true,
-      todayHighlight: true,
-      showMeridian: true,
-      pickerPosition: "bottom-left",
-      language: 'zh-CN',
-      startView: 2,
-      minView: 2
-  });
-  var today = new Date();
-  var nowdate = (today.getFullYear()) + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  var date = new Date(nowdate);
-  var mon = date.getMonth() + 1;
-  var day = date.getDate();
-  var mydate = date.getFullYear() + "-" + (mon < 10 ? "0" + mon : mon) + "-" + (day < 10 ? "0" + day : day);
- // document.getElementById("nowdate").value = mydate;
- 
-  $(".dropdown-toggle").dropdown('toggle');
 });
