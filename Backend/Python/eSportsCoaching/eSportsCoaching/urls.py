593eabe05d7cc7664d40972f6ec45aa55e10cfb9
Qiao Zhang <qiao.zhang.qz@gmail.com>
2017-12-03 13:30:40 +0800

    Summary

--- Backend/Python/eSportsCoaching/eSportsCoaching/urls.py
+++ Backend/Python/eSportsCoaching/eSportsCoaching/urls.py
@@ -19,3 +19,11 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
