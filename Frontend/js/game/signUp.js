ce364ed648c6aaff42f72d5152ebb8eae3bdebc6
Qu Xiao Ya <33386378+amelia278@users.noreply.github.com>
2017-12-04 21:26:05 +0800

    update signUp
    update signUp

---
+++ Frontend/js/game/signUp.js
@@ -0,0 +1,33 @@
+﻿var eyeIcon = document.getElementById('eyeIcon');
+var userPwd = document.getElementById('userPwd');
+var getVerifyCode = document.getElementById('getVerifyCode');
+eyeIcon.style.color = '#e3e3e4';
+var flag = false;
+var intervalFlag;
+var count = 60;
+eyeIcon.onclick = function () {
+    if (flag) {
+        userPwd.type = 'password';
+        flag = false;
+        eyeIcon.style.color = '#e3e3e4';
+    } else {
+        userPwd.type = 'text';
+        flag = true;
+        eyeIcon.style.color = '#9d9d9d';
+    }
+};
+
+getVerifyCode.onclick = function () {
+    getVerifyCode.value = count;
+    intervalFlag = setInterval(function () {
+        count--;
+        getVerifyCode.value = count;
+        if (count === 0) {
+            clearInterval(intervalFlag);
+            getVerifyCode.value = '获取验证码';
+            count = 60;
+        }
+    }, 1000);
+
+}
+
