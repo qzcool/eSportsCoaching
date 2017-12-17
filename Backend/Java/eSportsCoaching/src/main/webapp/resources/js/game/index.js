ce9c6f6af76fa6a757bcd5253126f97b9affff27
Qu Xiao Ya <33386378+amelia278@users.noreply.github.com>
2017-12-17 13:47:13 +0800

    update backEnd file

---
+++ Backend/Java/eSportsCoaching/src/main/webapp/resources/js/game/index.js
@@ -0,0 +1,12 @@
+var portraitImage=["player1.png","player2.png","player3.png"]
+function updateProfile(){
+  var playerPortrait=document.getElementById("player-portrait");
+
+}
+
+document.getElementById('searchSensei').onclick = function(){
+    window.location.href = 'gamer/searchSensei.html';
+};
+document.getElementById('searchWiki').onclick = function(){
+  window.location.href = 'gameWiki.html';
+}
