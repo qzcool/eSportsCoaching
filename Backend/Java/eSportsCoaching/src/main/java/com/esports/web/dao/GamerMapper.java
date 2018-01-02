a3d2b091804439b8f9a7c87fd5b0d7fea43aff30
XJD <jiadong.xu@fengjr.com>
2018-01-02 21:55:49 +0800

    增加用户profile更新功能

---
+++ Backend/Java/eSportsCoaching/src/main/java/com/esports/web/dao/GamerMapper.java
@@ -0,0 +1,12 @@
+package com.esports.web.dao;
+
+import java.util.Map;
+
+/**
+ * Description:
+ * Author: XJD
+ * Date: 2018/01/02
+ */
+public interface GamerMapper {
+    void updateProfile(Map<String,Object> params);
+}
